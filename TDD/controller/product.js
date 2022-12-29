5e144459362bc03f8cc6787e872aeafc21a0e45e
SOPLAY <tkdgns1341@gmail.com>
2022-12-29 14:54:58 +0900

    TDD - create 기본 테스트용 로직 구현

--- TDD/controller/product.js
+++ TDD/controller/product.js
@@ -1,6 +1,11 @@
 const productModel = require('../models/Product');
 
-exports.createProduct = (req, res, next) => {
-  const createedProduct = productModel.create(req.body);
-  res.status(201).json(createedProduct);
+exports.createProduct = async (req, res, next) => {
+  try {
+    const createedProduct = await productModel.create(req.body);
+    console.log('createedProduct', createedProduct);
+    res.status(201).json(createedProduct);
+  } catch (error) {
+    next(error);
+  }
 };
