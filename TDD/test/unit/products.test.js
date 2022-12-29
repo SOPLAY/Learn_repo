3a0a14e8a7a7aac5403d2a697847cd63142d9003
SOPLAY <tkdgns1341@gmail.com>
2022-12-29 14:55:35 +0900

    TDD - 비동기 로직 작성

--- TDD/test/unit/products.test.js
+++ TDD/test/unit/products.test.js
@@ -10,7 +10,7 @@ let req, res, next;
 beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
-  next = null;
+  next = jest.fn();
 });
 
 describe('Product Controller Create', () => {
@@ -23,21 +23,31 @@ describe('Product Controller Create', () => {
     expect(typeof productController.createProduct).toBe('function');
   });
 
-  it('should call ProductModel.create', () => {
-    productController.createProduct(req, res, next);
+  it('should call ProductModel.create', async () => {
+    await productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
   });
 
-  it('should return 201 response code', () => {
-    productController.createProduct(req, res, next);
+  it('should return 201 response code', async () => {
+    await productController.createProduct(req, res, next);
     expect(res.statusCode).toBe(201);
+
     //성공적으로 통신이 끝났을 때
     expect(res._isEndCalled()).toBeTruthy();
   });
 
-  it('should return json body in response', () => {
+  it('should return json body in response', async () => {
     productModel.create.mockReturnValue(newProduct);
-    productController.createProduct(req, res, next);
+    await productController.createProduct(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
+
+  it('에러 헨들링', async () => {
+    const errorMessage = { message: 'description property is missing' };
+
+    const rejectedPromise = Promise.reject(errorMessage);
+    productModel.create.mockReturnValue(rejectedPromise);
+    await productController.createProduct(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
   });
 });
