35165553114a67c8b83199ba739e7caee5b6cb80
SOPLAY <tkdgns1341@gmail.com>
2022-12-31 19:46:58 +0900

    TDD - getProducts 단위 테스트 작성

--- TDD/test/unit/products.test.js
+++ TDD/test/unit/products.test.js
@@ -2,6 +2,7 @@ const productController = require('../../controller/product');
 const productModel = require('../../models/Product');
 const httpMocks = require('node-mocks-http');
 const newProduct = require('../data/new-product.json');
+const allProduct = require('../data/all-products.json');
 
 productModel.create = jest.fn();
 productModel.find = jest.fn();
@@ -61,5 +62,17 @@ describe('Product Controller Get', () => {
   it('should call ProductModal.find({})', async () => {
     await productController.getProducts(req, res, next);
     expect(productModel.find).toHaveBeenCalledWith({});
+  });
+
+  it('should return 200 response', async () => {
+    await productController.getProducts(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled).toBeTruthy();
+  });
+
+  it('should return json body in response', async () => {
+    productModel.find.mockReturnValue(allProduct);
+    await productController.getProducts(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(allProduct);
   });
 });
