9f96ef89997aef3656d026270b4a4bfd6864c4a3
SOPLAY <tkdgns1341@gmail.com>
2023-01-01 18:45:36 +0900

    TDD - updateProduct 통합 test code 작성

--- TDD/test/intergration/products.int.test.js
+++ TDD/test/intergration/products.int.test.js
@@ -48,3 +48,20 @@ it('GET id doenst exist /api/products/:productId ', async () => {
   const res = await request(app).get(`/api/products/63ac5fef30fed75cf154f093`);
   expect(res.statusCode).toBe(404);
 });
+
+it('PUT /api/products/:productId', async () => {
+  const res = await request(app)
+    .put(`/api/products/${firstProduct._id}`)
+    .send({ name: 'updated name', description: 'updated description' });
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.name).toBe('updated name');
+  expect(res.body.description).toBe('updated description');
+});
+
+it('should return 404 on PUT /api/products', async () => {
+  const res = await request(app)
+    .put(`/api/products/63ac5fef30fed75cf154f019`)
+    .send({ name: 'updated name', description: 'updated description' });
+  expect(res.statusCode).toBe(404);
+});
