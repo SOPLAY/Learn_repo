e3205500c80982897f8f3766d1ce66725fffdad4
SOPLAY <tkdgns1341@gmail.com>
2023-01-01 16:42:51 +0900

    TDD - getProductById 모듈 test code 작성

--- TDD/test/unit/products.test.js
+++ TDD/test/unit/products.test.js
@@ -113,4 +113,12 @@ describe('Product Controller GetById ', () => {
     expect(res.statusCode).toBe(404);
     expect(res._isEndCalled).toBeTruthy();
   });
+
+  it('should handle errors', async () => {
+    const errorMessage = { message: 'error' };
+    const rejectedPromise = Promise.reject(errorMessage);
+    productModel.findById.mockReturnValue(rejectedPromise);
+    await productController.getProductById(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
 });
